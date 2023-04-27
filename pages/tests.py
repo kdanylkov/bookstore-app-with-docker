76ab83afdc3cbc7d9f6fcf9d64a265faf68e11fb
Kirill Danilkov <k.danilkov.work@gmail.com>
2023-04-27 21:30:29 +0300

    add:about page, crispy forms

--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
 class HomePageTestCase(SimpleTestCase):
@@ -20,7 +20,7 @@ class HomePageTestCase(SimpleTestCase):
         self.assertTemplateUsed(self.response, 'home.html')
 
     def test_homepage_content(self):
-        self.assertContains(self.response, 'our home page')
+        self.assertContains(self.response, 'is home page')
 
     def test_homepage_content_wrong(self):
         self.assertNotContains(self.response,
@@ -29,3 +29,22 @@ class HomePageTestCase(SimpleTestCase):
     def test_homepage_url_resolves_homepageview(self):
         view = resolve('/')
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+
+class AboutPageTestCase(SimpleTestCase):
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'About Page')
+
+    def test_aboutpage_doesnot_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'This is some random staff')
+
+    def test_aboutpage_exist_at_expected_location(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
