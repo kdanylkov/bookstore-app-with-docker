df5c81b34c6f232510f933563ba3c924b91118c6
Kirill Danilkov <k.danilkov.work@gmail.com>
2023-05-01 16:15:22 +0300

    search fun-ty added

--- books/views.py
+++ books/views.py
@@ -1,9 +1,9 @@
 from django.views.generic import ListView, DetailView
-from django.db.models import Prefetch
+from django.db.models import Prefetch, Q
 from django.contrib.auth.mixins import (
-        LoginRequiredMixin,
-        PermissionRequiredMixin
-        )
+    LoginRequiredMixin,
+    PermissionRequiredMixin
+)
 
 from .models import Book, Review
 
@@ -17,12 +17,26 @@ class BookListView(LoginRequiredMixin, ListView):
 
 class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     queryset = Book.objects.all().prefetch_related(
-            Prefetch(
-                'reviews',
-                queryset=Review.objects.all().select_related('author')
-                )
-            )
+        Prefetch(
+            'reviews',
+            queryset=Review.objects.all().select_related('author')
+        )
+    )
     template_name = 'books/book_detail.html'
     context_object_name = 'book'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Book.objects.filter(
+                Q(title__icontains=query) | Q(author__icontains=query)
+            )
+        else:
+            return []
